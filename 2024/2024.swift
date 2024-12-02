b7bf95ea17a53643c2e60c1d2b5ace32e83acf83
Ilya Grechuhin <i.grechuhin@gmail.com>
2024-12-02 13:02:39 +0300

    2024.Day2

--- 2024/2024.swift
+++ 2024/2024.swift
@@ -4,5 +4,8 @@ enum AoC2024: AoC {
     static func run() {
         assert(solveDay1Puzzle1() == 1189304)
         assert(solveDay1Puzzle2() == 24349736)
+        
+        assert(solveDay2Puzzle1() == 359)
+        assert(solveDay2Puzzle2() == 418)
     }
 }
